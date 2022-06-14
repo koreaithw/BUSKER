27220eace3d3923a367498a8195b8784f577ef07
wanggoNya <tndus6384@naver.com>
2022-06-15 02:38:28 +0900

    0614 Persistance 파트입니다. VO, Mapper 작성 완료 했습니다.

---
+++ src/main/java/com/example/teamprojeect/domain/vo/ShowReplyPageDTO.java
@@ -0,0 +1,18 @@
+package com.example.teamprojeect.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShowReplyPageDTO {
+    private List<ShowReplyVO> list;
+    private int total;
+}
---
+++ src/main/java/com/example/teamprojeect/mapper/ArtistMapper.java
@@ -0,0 +1,34 @@
+package com.example.teamprojeect.mapper;
+
+import com.example.teamprojeect.domain.vo.ArtistVO;
+import com.example.teamprojeect.domain.vo.Criteria;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ArtistMapper {
+    // 아티스트 등록 신청
+    public void insertApply(ArtistVO artistVO);
+
+    // 아티스트 정보 수정 신청
+    public int updateApply(ArtistVO artistVO);
+
+    // 아티스트 삭제
+    public int delete(Long artistNumber);
+
+    // 아티스트 전체 개수
+    public int getTotal();
+
+    // 아티스트 목록 (전체)
+    public List<ArtistVO> getListAll(Criteria criteria);
+
+    // 아티스트 목록 (뮤지션, 퍼포먼스 구분)
+    public List<ArtistVO> getListType(Criteria criteria, Long artistType);
+
+    // 아티스트 상세 정보
+    public ArtistVO getDetail(Long artistNumber);
+
+    // 아티스트 검색
+
+}
