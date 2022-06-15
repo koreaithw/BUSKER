6926d5c3f053cb0772942d0fc4d3b65ed401e57c
alsgud2659 <alsgud2659@naver.com>
2022-06-16 00:40:12 +0900

    컨트롤러 경로 수정 및 DAO 작성

--- src/main/java/com/example/teamprojeect/service/LikeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.teamprojeect.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class LikeService {
-}
