7bed73556ee36195eb927194f07035b592d49aef
wanggoNya <tndus6384@naver.com>
2022-06-15 15:57:22 +0900

    0615 김수연 Persistance mapper 인터페이스 메서드 명 정의

--- src/main/java/com/example/teamprojeect/domain/vo/list/ListDTO.java
+++ src/main/java/com/example/teamprojeect/domain/vo/list/ListDTO.java
@@ -1,4 +1,20 @@
 package com.example.teamprojeect.domain.vo.list;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@NoArgsConstructor
 public class ListDTO {
+    private String artistType;
+    private String sortingType;
+    private String sortingDate;
+    private String ingWhether;
+    private String showType;
+    private String donationType;
+    private String likeType;
+    private String tag;
+
 }
