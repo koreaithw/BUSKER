814b97b76088f57b3546c69729986a128a87b24e
ckshin29 <shinchangkyun@gmail.com>
2022-06-17 16:39:41 +0900

    아티스트 매퍼 쿼리 작성

--- src/test/java/com/example/teamprojeect/mapper/artist/ArtistMapperTests.java
+++ src/test/java/com/example/teamprojeect/mapper/artist/ArtistMapperTests.java
@@ -1,9 +1,45 @@
 package com.example.teamprojeect.mapper.artist;
 
+import com.example.teamprojeect.domain.vo.artist.ArtistVO;
+import com.example.teamprojeect.domain.vo.list.ListDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
 public class ArtistMapperTests {
+
+    @Autowired
+    private ArtistMapper artistMapper;
+
+    //최신순 정렬 테스트
+//    @Test
+//    public void getListTest(){
+//        artistMapper.getList(new ListDTO("A","1","2","new","3"))
+//
+//                .stream().map(ArtistVO::toString).forEach(log::info);
+//    }
+
+    @Test
+    public void getListTest(){
+        artistMapper.getList(new ListDTO("A","1","2","NEW","3"))
+
+                .stream().map(ArtistVO::toString).forEach(log::info);
+    }
+
+    @Test
+    public void getListMusicianTest(){
+        artistMapper.getListMusician(new ListDTO("A","1","2","NEW","3"))
+
+                .stream().map(ArtistVO::toString).forEach(log::info);
+    }
+
+    @Test
+    public void getListPerformanceTest(){
+        artistMapper.getListPerformance(new ListDTO("A","1","2","NEW","3"))
+
+                .stream().map(ArtistVO::toString).forEach(log::info);
+    }
 }
