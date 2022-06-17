5293d4dfb15a93c752622005c8ad5471f2f7d938
alsgud2659 <alsgud2659@naver.com>
2022-06-17 11:43:12 +0900

    WorkDAO 테스트

--- src/test/java/com/example/teamprojeect/domain/dao/work/WorkDaoTests.java
+++ src/test/java/com/example/teamprojeect/domain/dao/work/WorkDaoTests.java
@@ -1,8 +1,11 @@
 package com.example.teamprojeect.domain.dao.work;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
 public class WorkDaoTests {
+    @Autowired
+    private WorkDAO workDAO;
 }
