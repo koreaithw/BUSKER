45a439da24ff5026e29406c99df61dcac4f99cbd
Ohgangrae <oasisoyw@naver.com>
2022-06-17 23:57:20 +0900

    헤더 고쳤습니다

--- src/test/java/com/example/teamprojeect/mapper/artist/ArtistMapperTests.java
+++ src/test/java/com/example/teamprojeect/mapper/artist/ArtistMapperTests.java
@@ -1,9 +1,19 @@
 package com.example.teamprojeect.mapper.artist;
 
+import com.example.teamprojeect.domain.vo.paging.Criteria;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
 public class ArtistMapperTests {
+//    @Autowired
+//    private ArtistMapper artistMapper;
+
+//    @Test
+//    public void getListTest(){
+//        ArtistMapper.getList(new Criteria(1,10), new listDTO());
+//    }
 }
