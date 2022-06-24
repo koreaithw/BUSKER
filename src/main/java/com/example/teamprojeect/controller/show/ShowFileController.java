3b12eae5c5995b8ef9d795ae4a6a9d0b1fe53fe2
wanggoNya <tndus6384@naver.com>
2022-06-24 09:08:10 +0900

    0624 파일처리중

--- src/main/java/com/example/teamprojeect/controller/show/ShowFileController.java
+++ src/main/java/com/example/teamprojeect/controller/show/ShowFileController.java
@@ -3,6 +3,7 @@ package com.example.teamprojeect.controller.show;
 import com.example.teamprojeect.service.ShowService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,7 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ShowFileController {
     // 필드 생성
-    private ShowService concertService;
+    @Autowired
+    private ShowService showService;
 
+    // upload
+
+    // display
+
+    // download
+
+    // delete
+
+    // list
 
 }
