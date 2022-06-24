999de2691c8f8d600678b14f03fbbd5820f0c1b0
Jiyeon1104 <2018110325@dgu.ac.kr>
2022-06-24 23:24:35 +0900

    * 0624 후원 목록, 아티스트 계정 등록, 아티스트 계정 정보 수정, 아티스트 계정 탈퇴 *

--- src/main/java/com/example/teamprojeect/mapper/donation/DonationMapper.java
+++ src/main/java/com/example/teamprojeect/mapper/donation/DonationMapper.java
@@ -2,11 +2,20 @@ package com.example.teamprojeect.mapper.donation;
 
 
 import com.example.teamprojeect.domain.vo.donation.DonationVO;
+import com.example.teamprojeect.domain.vo.list.ListDTO;
+import com.example.teamprojeect.domain.vo.paging.Criteria;
 import com.example.teamprojeect.domain.vo.show.ShowVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface DonationMapper {
 
+    // 후원 등록
     public void insert(DonationVO donationVO);
+
+    // 후원 목록 불러오기
+    public List<DonationVO> donationList(@Param("criteria") Criteria criteria, @Param("artistNumber") Long artistNumber, @Param("listDTO")ListDTO listDTO);
 }
