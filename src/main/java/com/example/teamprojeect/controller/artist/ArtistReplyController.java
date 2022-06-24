d252676aa4db8b209ff222b90a57bbd2928d7902
ckshin29 <shinchangkyun@gmail.com>
2022-06-24 11:34:20 +0900

    아티스트 정보 댓글

--- src/main/java/com/example/teamprojeect/controller/artist/ArtistReplyController.java
+++ src/main/java/com/example/teamprojeect/controller/artist/ArtistReplyController.java
@@ -1,11 +1,24 @@
 package com.example.teamprojeect.controller.artist;
 
 
+import com.example.teamprojeect.domain.vo.artist.ArtistReplyVO;
+import com.example.teamprojeect.domain.vo.paging.Criteria;
+import com.example.teamprojeect.domain.vo.paging.artist.ArtistReplyPageDTO;
+import com.example.teamprojeect.domain.vo.paging.show.ShowReplyPageDTO;
+import com.example.teamprojeect.domain.vo.show.ShowReplyVO;
 import com.example.teamprojeect.service.ArtistService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -13,19 +26,61 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/reply/artist/*")
 public class ArtistReplyController {
     // 필드 생성
+    @Autowired
     private ArtistService artistService;
 
+    // 댓글 목록
+    @GetMapping("/replyList/{artistNumber}/{page}")
+    public ArtistReplyPageDTO getListReply(@PathVariable("artistNumber") Long artistNumber, @PathVariable("page") int pageNum) {
+        List<ArtistReplyVO> artistReplyList = artistService.getListReply(new Criteria(pageNum, 10), artistNumber);
 
-    // 아티스트에게 달린 댓글 목록
+        artistReplyList.forEach(artistReplyVO -> {
+            try {
+                SimpleDateFormat rowFormate = new SimpleDateFormat("yyyy-MM-dd HH24:mm:ss");
+                SimpleDateFormat dayFormat = new SimpleDateFormat("yyyy.MM.dd a HH:mm");
+                String replyRegisterDate = artistReplyVO.getReplyRegisterDate();
+                String replyUpdateDate = artistReplyVO.getReplyUpdateDate();
 
+                Date replyRegisterDateParse = rowFormate.parse(replyRegisterDate);
+                Date replyUpdateDateParse = dayFormat.parse(replyUpdateDate);
 
-    // 아티스트에게 댓글 달기
+                replyRegisterDate = rowFormate.format(replyRegisterDateParse);
+                replyUpdateDate = dayFormat.format(replyUpdateDateParse);
 
+                artistReplyVO.setReplyRegisterDate(replyRegisterDate);
+                artistReplyVO.setReplyUpdateDate(replyUpdateDate);
+            } catch (ParseException e) {
+                System.err.println("dateStr : " + ", datePattern:");
+                e.printStackTrace();
+            }
+        });
+        return new ArtistReplyPageDTO(artistReplyList, artistService.getTotalReply(artistNumber));
+    }
 
-    // 댓글 삭제
+    // 댓글 입력
+    @PostMapping("/replyNew")
+    public ResponseEntity<String> createReply(@RequestBody ArtistReplyVO artistReplyVO) throws UnsupportedEncodingException {
+        artistService.registerReply(artistReplyVO);
+        return new ResponseEntity<>(new String("댓글 등록에 성공하였습니다.".getBytes(), "UTF-8"), HttpStatus.OK);
+    }
 
 
     // 댓글 수정
+    @PostMapping("/{replyNumber}")
+    public String modifyReply(@PathVariable("replyNumber") Long replyNumber, @RequestBody ArtistReplyVO artistReplyVO) {
+        artistReplyVO.setReplyNumber(replyNumber);
+        artistService.modifyReply(artistReplyVO);
+        return "댓글이 수정되었습니다.";
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/{replyNumber}")
+    public String removeReply(@PathVariable("replyNumber") Long replyNumber) {
+        artistService.removeReply(replyNumber);
+        return "댓글이 삭제되었습니다.";
+    }
+
+
 
 
 
--- src/main/java/com/example/teamprojeect/service/ArtistService.java
+++ src/main/java/com/example/teamprojeect/service/ArtistService.java
@@ -5,9 +5,11 @@ import com.example.teamprojeect.domain.dao.artist.ArtistFileDAO;
 import com.example.teamprojeect.domain.dao.artist.ArtistReplyDAO;
 import com.example.teamprojeect.domain.dao.artist.PaymentDAO;
 import com.example.teamprojeect.domain.dao.user.LikeDAO;
+import com.example.teamprojeect.domain.vo.artist.ArtistReplyVO;
 import com.example.teamprojeect.domain.vo.artist.ArtistVO;
 import com.example.teamprojeect.domain.vo.list.ListDTO;
 import com.example.teamprojeect.domain.vo.paging.Criteria;
+import com.example.teamprojeect.domain.vo.show.ShowReplyVO;
 import com.example.teamprojeect.domain.vo.show.ShowVO;
 import com.example.teamprojeect.mapper.artist.ArtistMapper;
 import lombok.RequiredArgsConstructor;
@@ -51,6 +53,26 @@ public class ArtistService {
     //아티스트 상세정보
     public ArtistVO getDetail(Long artistNumber) {
         return artistDAO.getDetail(artistNumber);
+    }
+
+    public void registerReply(ArtistReplyVO artistReplyVO) {
+        artistReplyDAO.register(artistReplyVO);
+    }
+
+    public boolean modifyReply(ArtistReplyVO artistReplyVO) {
+        return artistReplyDAO.modify(artistReplyVO);
+    }
+
+    public boolean removeReply(Long replyNumber) {
+        return artistReplyDAO.remove(replyNumber);
+    }
+
+    public int getTotalReply(Long artistNumber) {
+        return artistReplyDAO.getTotal(artistNumber);
+    }
+
+    public List<ArtistReplyVO> getListReply(Criteria criteria, Long showNumber) {
+        return  artistReplyDAO.getList(criteria, showNumber);
     }
 
 
